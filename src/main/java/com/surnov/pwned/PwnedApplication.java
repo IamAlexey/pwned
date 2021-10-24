8c29bf9284f2adeb674fbeaa1c568bcd8cad7c51
Alex <alexey.surnov@gmail.com>
2021-10-24 22:53:56 +0300

    Initial commit

---
+++ src/main/java/com/surnov/pwned/PwnedApplication.java
@@ -0,0 +1,13 @@
+package com.surnov.pwned;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwnedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwnedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surnov/pwned/PwnedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surnov.pwned;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwnedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
